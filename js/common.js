9632f998e0cece6ce7015d285b32d25c2fa94358
ITkah <ilatkacenko132@gmail.com>
2020-01-26 09:43:29 +0200

    end

--- js/common.js
+++ js/common.js
@@ -55,22 +55,69 @@ $(document).ready(function() {
         }
     });
 
+$(".curse-change").on("click", function (e) {
+        e.preventDefault();
+        let curse = $(this).attr("data-curse");
+        $(".curse-input").val(curse);
+    });
+
     $("#feedback").submit(function() {
         var form_data = $(this).serializeArray();
         $.ajax({
             type: "POST",
-            url: "/php/sendMail.php",
+            url: "../../mail.php",
             data: form_data,
             success: function() {
                 $(".call-thank").click();
                 console.log("send");
             },
-            error: function(data) {
+            error: function() {
                 $(".call-thank").click();
+                alert("Ошибка");
                 console.log("error");
             }
         });
 
         return false;
     });
+
+
+        $(".menu-ikor").on("click","a", function (event) {
+            event.preventDefault();
+            if ($(window).width() < 990) {
+                $("header").removeClass("header-active");
+                $(".menu-active").removeClass("menu-close");
+                $("nav").slideUp("fast");
+            }
+            var id  = $(this).attr('href'),
+                top = $(id).offset().top - 125;
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+
+
+    $('.phons').on('input', function () {
+        if (this.value != this.value.replace(/[^0-9\.]/g, '')) {
+            this.value = this.value.replace(/[^0-9\.]/g, '');
+        }
+        return false;
+    });
+
+    $(function() {
+        menu_top = $('.header-menu').offset().top;        // запоминаем положение меню
+        $(window).scroll(function () {             // отслеживаем событие прокрутки страницы
+          if ($(window).scrollTop() > menu_top) {  // если прокрутка дошла до меню
+            if ($('.menu').css('position') != 'fixed') {  // проверяем, если меню еще не зафиксировано
+              $('.header-menu').addClass('header-fixed');
+              $('main').css('margin-top','80px'); // делаем отступ, чтобы контент не "скакал" в момент фиксации меню
+            }
+          } else {                                 // прокрутка страницы обратно вверх достигла место "перехода" меню
+            if ($('.header-menu').css('position') == 'fixed') {  // если меню зафиксировано
+              $('.header-menu').removeClass('header-fixed');
+              $('main').css('margin-top','');
+            }
+          }
+        });
+      });
+        
+        
 });
